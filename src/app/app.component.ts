c7f40bfb17531aaea913e8a6dc32c5a8936514d7
Nicolás Sivoff <nicosivoff@gmail.com>
2022-05-21 08:57:51 -0300

    Agrego contador de tiempo de juego

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,20 +10,28 @@ import { Cell } from './models/cell.model';
 })
 export class AppComponent {
   title: string = 'buscaminas';
-  size: number = 15;
-  mines: number = 3;
+  size: number = 10;
+  mines: number = 7;
   board: Board = new Board(this.size, this.mines);
+  counter: number = 0;
+  displayTime: any = '00:00';
+  interval: any;
+  firstClick: boolean = false;
   constructor() {}
-
-  ngOnInit(): void {}
 
   onClickCell(cell: Cell) {
     const result = this.board.onClickCell(cell);
+    if (!this.firstClick) {
+      this.startCountDown();
+      this.firstClick = true;
+    }
     if (result == GameResultEnum.LOST_GAME) {
       // Loser
+      clearInterval(this.interval);
       alert('LOSER');
     } else if (result == GameResultEnum.WON_GAME) {
       // Winner
+      clearInterval(this.interval);
       alert('WINNER');
     }
   }
@@ -48,6 +56,30 @@ export class AppComponent {
   }
 
   restartGame(): void {
+    this.counter = 0;
+    this.displayTime = '00:00';
+    clearInterval(this.interval);
+    this.firstClick = false;
     this.board = new Board(this.size, this.mines);
+  }
+
+  startCountDown() {
+    this.counter = 0;
+    this.interval = setInterval(() => {
+      if (this.counter === 0) {
+        this.counter++;
+      } else {
+        this.counter++;
+      }
+      this.displayTime = this.transform(this.counter);
+    }, 1000);
+  }
+
+  transform(value: number): string {
+    var minutes: any = Math.floor(value / 60);
+    var seconds: any = value - minutes * 60;
+    if (minutes < 10) minutes = '0' + minutes;
+    if (seconds < 10) seconds = '0' + seconds;
+    return minutes + ':' + seconds;
   }
 }
